0cf3c703c940e4b5d4445340b847a503abe5de00
Илья Козлов <ilyakozlov@MacBook-Pro-Ila.local>
2020-04-06 21:52:20 +0300

    Tasks 1-3 are completed

--- RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
+++ RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
@@ -2,6 +2,72 @@
 
 @implementation PolynomialConverter
 - (NSString*)convertToStringFrom:(NSArray <NSNumber*>*)numbers {
-    return @"";
+    
+    if([numbers count] == 0) {
+    return nil;
+    }
+    
+    NSArray *array = [numbers copy];
+    
+    int sizeOfArr=(int)[array count];
+    NSInteger curValue;
+    NSInteger exponent = 0;
+    NSMutableString *result = [NSMutableString new];
+    BOOL firstIteration=YES;
+    for(int i=0;i<sizeOfArr;i++)
+    {
+        exponent=sizeOfArr-1-i;
+        curValue=[array[i] integerValue];
+    
+        if(firstIteration)
+        {
+            
+            if(curValue<0)
+            {
+                [result appendString:@"- "];
+                curValue*=-1;
+            }
+            else if(curValue==0)
+            {
+                continue;
+            }
+      }
+        else{
+            if(curValue>0)
+            {
+                [result appendString:@" + "];
+            }
+            else if(curValue<0){
+                [result appendString:@" - "];
+                curValue*=-1;
+            }
+            else if(curValue==0){
+                continue;
+            }
+        }
+        
+        firstIteration=NO;
+        
+        if(exponent!=0)
+        {
+            
+            if(curValue!=1){
+                [result appendFormat:@"%ld",(long)curValue];
+                }
+            [result appendFormat:@"x"];
+            if(exponent!=1){
+                if(curValue!=1){
+                [result appendFormat:@"^%ld",(long)exponent];
+                }
+            }
+           
+        }
+        else{
+            [result appendFormat:@"%ld",(long)curValue];
+        }
+    }
+    
+    
+    return result;
 }
 @end
--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -1,7 +1,97 @@
 #import "Combinator.h"
 
 @implementation Combinator
-- (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
-    return @0;
+
+
+/*- (long)factorial: (long)chislo{
+    if (chislo == 1){
+        return 1;
+    }else{
+        return [self factorial:chislo-1] * chislo;
+        
+    }
+    
 }
-@end
+
+-(double)factorial:(long)chislo{
+    long fact=1;
+    if(chislo==1)
+    {
+        return fact;
+    }
+    else
+    {
+        
+        for(int i=1;i<=chislo;++i)
+        {
+           
+            fact*=i;
+        }
+        return fact;
+    }
+}
+- (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
+    if (array[1].intValue == array[0].intValue) { return @1;
+    }
+   // if([array count]==2){
+    
+    int m=[array[0] intValue];
+    int n=[array[1]intValue];
+    //long k=1;
+    int sochet;
+        for(int k=1;k<n;k++){
+   // while(k<n){
+        sochet=[self factorial:n]/[self factorial:n-k]*[self factorial:k];
+        if(sochet==m)
+        {
+            return [NSNumber numberWithInt:k];
+            break;
+        }
+     //   ++k;
+    }
+        return nil;
+   /* }
+    else
+    {
+        return nil;
+    }
+  
+}*/
+- (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
+    
+    NSNumber *result = nil;
+    
+    long m = [array[0] intValue];
+    
+    long n  = [array[1] intValue];
+    
+    long sochet;
+    
+    for (int x = 1; x < n; x++) {
+        
+        sochet = [self factorial:n] / ([self factorial:x] * [self factorial:(n - x)]);
+        
+        if (sochet == m) {
+            
+            result = @(x);
+            
+            break;
+        }
+    }
+    
+    return result;
+}
+
+- (long)factorial:(int)number {
+
+    double result = 1;
+
+    for (int i = 1; i <= number; i++) {
+        
+        result *= i;
+    }
+
+    return result;
+}
+    @end
+    
